26c39687480424bf3f215d8872d9ad6a9c049712
ddm <parksam105@gmail.com>
2023-04-17 18:09:29 +0900

    second

--- minter_front_back/client_react/src/App.tsx
+++ minter_front_back/client_react/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React react world!!
+          Welcome to React react world!! Now Test!!
         </p>
         <a
           className="App-link"
